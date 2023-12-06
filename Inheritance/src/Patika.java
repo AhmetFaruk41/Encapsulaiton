f85879b057d75c551fb2c3faeb44d9cc833b1baa
Ahmet Faruk ÇATLAR <ahmetfaruk411@gmail.com>
2023-12-06 14:08:51 +0300

    kalıtım

---
+++ Inheritance/src/Patika.java
@@ -0,0 +1,7 @@
+public class Patika {
+    public static void main(String[] args) {
+        Calisan c1 = new Calisan("ahmet faruk","05463736238","ahmet@gmail.com");
+        Akademisyen a1 = new Akademisyen("ahmet faruk","05463736238","ahmet@gmail.com","CENG","prof");
+        a1.derseGir();
+    }
+}
